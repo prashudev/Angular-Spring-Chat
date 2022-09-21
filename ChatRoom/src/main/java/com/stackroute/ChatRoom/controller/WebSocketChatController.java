3b9a66791af19fa41d3e40a06dd422a29cc3a0bc
Prashu <Prashanth.P@stackroute.in>
2022-09-21 10:09:07 +0530

    first commit

---
+++ ChatRoom/src/main/java/com/stackroute/ChatRoom/controller/WebSocketChatController.java
@@ -0,0 +1,16 @@
+package com.stackroute.ChatRoom.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketChatController {
+
+    @MessageMapping("/resume")
+    @SendTo("/start/initial")
+    public String chat(String msg) {
+        System.out.println(msg);
+        return msg;
+    }
+}
